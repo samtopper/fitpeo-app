bdf09507361ef37b83999557c8bbe073ac2b7963
Sameer Ahmed Khan <sakhancse@gmail.com>
2023-10-24 20:25:58 +0530

    chart bar diagram - initial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import { Title } from "./components/title";
 import { List } from "./components/list";
 import { Header } from "./components/header";
+import { Profile } from "./components/profile/profile";
 
 import "./App.css";
 import { Cards } from "./components/cards";
+import { useState } from "react";
+import BarChart from "./components/barChart";
 
 function App() {
+  const [view, setView] = useState("dashboard");
+  const handleClick = (itemName) => {
+    console.log("handle click event", itemName);
+    setView(itemName);
+  };
+
   return (
     <div className="container">
       <div className="navigationSection">
         <Title />
-        <List />
+        <List handleClick={handleClick} />
+        <Profile />
       </div>
 
       <div className="viewSection">
         <Header username="John Doe" />
-        <Cards />
+        {view === "dashboard" ? (
+          <>
+            <Cards />
+            <div>overview section</div>
+            <BarChart />
+          </>
+        ) : (
+          <div>Page is under development</div>
+        )}
       </div>
     </div>
   );
